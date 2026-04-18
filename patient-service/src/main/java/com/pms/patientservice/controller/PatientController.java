5c641aa7515c38c6674ecc85ef31f4b74fdeb12b
Xinnan-Alex <leongxinnan@outlook.my>
2026-04-18 17:25:58 +0800

    feat: added in update patient feature for patient service

--- patient-service/src/main/java/com/pms/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pms/patientservice/controller/PatientController.java
@@ -2,12 +2,16 @@ package com.pms.patientservice.controller;
 
 import com.pms.patientservice.dto.PatientRequestDTO;
 import com.pms.patientservice.dto.PatientResponseDTO;
+import com.pms.patientservice.dto.validator.CreatePatientValidationGroup;
 import com.pms.patientservice.service.PatientService;
 import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
@@ -25,8 +29,15 @@ public class PatientController {
     }
 
     @PostMapping
-    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+    public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
         PatientResponseDTO patients = patientService.createPatient(patientRequestDTO);
         return ResponseEntity.ok().body(patients);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
+
+        return ResponseEntity.ok().body(patientResponseDTO);
     }
 }
---
+++ patient-service/src/main/java/com/pms/patientservice/dto/validator/CreatePatientValidationGroup.java
@@ -0,0 +1,4 @@
+package com.pms.patientservice.dto.validator;
+
+public interface CreatePatientValidationGroup {
+}
